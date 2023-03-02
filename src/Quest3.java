b9cfd1dc8d31dae8b05bb4d921503f24fa0830c0
Igor Vinoski <igor.vinoski2@gmail.com>
2023-03-02 17:08:19 -0300

    Enunciado

--- src/Quest3.java
+++ src/Quest3.java
@@ -12,6 +12,12 @@ import java.io.IOException;
 import java.math.BigDecimal;
 
 public class Quest3 {
+    /*
+    Dado um vetor que guarda o valor de faturamento diário de uma distribuidora, faça um programa, na linguagem que desejar, que calcule e retorne:
+• O menor valor de faturamento ocorrido em um dia do mês;
+• O maior valor de faturamento ocorrido em um dia do mês;
+• Número de dias no mês em que o valor de faturamento diário foi superior à média mensal.
+     */
     public static void main(String[] args) {
 
         DocumentBuilderFactory factory = DocumentBuilderFactory.newInstance();
